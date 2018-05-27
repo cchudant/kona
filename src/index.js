c00fcc715c219acb67b41e677b16b3d12329cf3b
SkyBeastMC <skybeastmc@gmail.com>
2018-05-27 20:03:03 +0200

    async/await udp

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ const { port, hostname } = urlParse(announce)
 
 const socket = dgram.createSocket('udp4')
 const msg = Buffer.from('is anyone there?')
-socket.send(msg, port, hostname, (...args) => {console.log(...args)})
+socket.send(msg, port, hostname, (...args) => {
+  console.log(...args)
+})
 
 socket.on('message', message => {
   console.log(message)
