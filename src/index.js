067e0e3039ea9b099451131b06b8af4b4f0798e1
SkyBeastMC <skybeastmc@gmail.com>
2018-05-27 19:43:55 +0200

    hello world in udp

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 const fs = require('fs')
-const torrent = fs.readFileSync('alice.torrent')
-console.log(torrent.toString())
+const bencode = require('bencode')
+const { parse: urlParse } = require('url')
+const dgram = require('dgram')
+
+const torrent = bencode.decode(fs.readFileSync('gimp-2.10.2-setup.exe.torrent'))
+
+const announce = torrent.announce.toString()
+const { port, hostname } = urlParse(announce)
+
+const socket = dgram.createSocket('udp4')
+const msg = Buffer.from('is anyone there?')
+socket.send(msg, port, hostname, (...args) => {console.log(...args)})
+
+socket.on('message', message => {
+  console.log(message)
+})
